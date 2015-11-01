c9bc9538a30d7560de2c456c5c5184a8b5f6f86b
Jeff Brys <jeffreybrys@gmail.com>
2015-11-01 12:43:56 -0800

    assignment 38 complete

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,10 +1,17 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import io.bloc.android.blocly.R;
 import io.bloc.android.blocly.ui.adapter.ItemAdapter;
@@ -12,17 +19,18 @@ import io.bloc.android.blocly.ui.adapter.ItemAdapter;
 /**
  * Created by jeffbrys on 10/24/15.
  */
-public class BloclyActivity extends Activity {
+public class BloclyActivity extends Activity implements ImageLoadingListener {
 
     private ItemAdapter itemAdapter;
+    ImageView recyclerViewBackgroundImage;
+    String imageURL = "https://i.imgur.com/PcC50MI.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
 
-        
-
+        ImageLoader.getInstance().loadImage(imageURL, this);
         itemAdapter = new ItemAdapter();
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_activity_blocly);
@@ -31,4 +39,21 @@ public class BloclyActivity extends Activity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(itemAdapter);
     }
+
+    @Override
+    public void onLoadingStarted(String imageUri, View view){}
+
+    @Override
+    public void onLoadingFailed(String imageUri, View view, FailReason failReason){}
+
+    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage){
+        recyclerViewBackgroundImage = (ImageView) findViewById(R.id.iv_rv_background);
+
+        if (imageUri.equals(imageURL)) {
+            recyclerViewBackgroundImage.setImageBitmap(loadedImage);
+        }
+    }
+
+    @Override
+    public void onLoadingCancelled(String imageUri, View view) {}
 }
