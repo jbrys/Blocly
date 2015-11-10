21a0576469624db16096ad14b7a609a645562e90
Jeff Brys <jeffreybrys@gmail.com>
2015-11-10 15:49:31 -0800

    Assignment 50 Complete

--- app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
+++ app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
@@ -2,17 +2,23 @@ package io.bloc.android.blocly.api.network;
 
 import android.util.Log;
 
-import java.io.BufferedReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 /**
  * Created by jeffbrys on 11/10/15.
  */
 public class GetFeedsNetworkRequest extends NetworkRequest {
 
+    final static String TAG = GetFeedsNetworkRequest.class.getSimpleName();
+
     String[] feedUrls;
+    int entries = 0;
+
 
     public GetFeedsNetworkRequest(String... feedUrls) {
         this.feedUrls = feedUrls;
@@ -22,23 +28,56 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
     public Object performRequest() {
         for (String feedUrlString : feedUrls) {
             InputStream inputStream = openStream(feedUrlString);
+
+
             if (inputStream == null) {
                 return null;
             }
             try {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line = bufferedReader.readLine();
-                while (line != null) {
-                    Log.v(getClass().getSimpleName(), "Line: " + line);
-                    line = bufferedReader.readLine();
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+
+                xpp.setInput(inputStream, "UTF_8");
+
+                boolean insideItem = false;
+
+                int eventType = xpp.getEventType();
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                            entries++;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem)
+                                Log.v(TAG, xpp.nextText());
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+                            if (insideItem)
+                                Log.v(TAG, xpp.nextText());
+                        }
+                    } else if (eventType == XmlPullParser.END_TAG &&
+                            xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
+                    }
+
+                    eventType = xpp.next();
                 }
-                bufferedReader.close();
+
             } catch (IOException e) {
                 e.printStackTrace();
                 setErrorCode(ERROR_IO);
                 return null;
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();;
+                setErrorCode(ERROR_MALFORMED_XML);
+                return null;
             }
+
+            Log.v(TAG, "Entry count: " + entries);
         }
         return null;
     }
+
+
+
 }
