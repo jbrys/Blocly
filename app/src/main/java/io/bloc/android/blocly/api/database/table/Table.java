40bce0ae8d772bc361dc3f4ddadd95db558580ba
Jeff Brys <jeffreybrys@gmail.com>
2015-11-10 23:27:05 -0800

    Checkpoint 51 Complete

---
+++ app/src/main/java/io/bloc/android/blocly/api/database/table/Table.java
@@ -0,0 +1,19 @@
+package io.bloc.android.blocly.api.database.table;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by jeffbrys on 11/10/15.
+ */
+public abstract class Table {
+
+    protected static final String COLUMN_ID = "id";
+
+    public abstract String getName();
+
+    public abstract String getCreateStatement();
+
+    public void onUpgrade(SQLiteDatabase writableDatabase, int oldVersion, int newVersion) {
+
+    }
+}
